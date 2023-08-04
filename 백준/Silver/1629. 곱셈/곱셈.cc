d92960373160a4cc439950bd1ff2d2fcf25a41b3
csh-scl <140063021+csh-scl@users.noreply.github.com>
2023-08-04 21:02:38 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+ll func(ll a, ll b, ll c) {
+	if (b == 1)
+	{
+		return a%c;
+	}
+	
+	long long val = func(a, b / 2, c);
+	val = val * val % c;
+	if (b % 2 == 0) return val;
+	return val* a % c;
+}
+
+int main(void) {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	ll a, b, c;
+	cin >> a >> b >> c;
+	ll result = func(a, b, c);
+	cout << result;
+}
