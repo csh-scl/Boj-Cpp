2c0f7f32c60d2bd477954cec470c37ddb58b306e
csh-scl <140063021+csh-scl@users.noreply.github.com>
2023-08-05 17:30:31 +0900

    [Silver I] Title: 스타트링크, Time: 24 ms, Memory: 6904 KB -BaekjoonHub

---
+++ 백준/Silver/5014. 스타트링크/스타트링크.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// a,b,c,d,e  a가 총 개수 b가 시작층 c가 도착층 d 위로 올라가는 개수 e 가 내려오는 개수
+// 종료조건은 현재상태가 c가 되면 되겠다 그전까지는 bfs를 돌리는데 최고 가서 u 터지리고 지하 뚤어도 터트리고 
+int tower[1000002];
+bool val[1000002];
+
+int main(void) {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int a, b, c, d, e;
+	cin >> a >> b >> c >> d >> e;
+	queue<int> q;
+	q.push(b);
+	val[b] = true;
+	while (!q.empty()) {
+		int cur = q.front();
+		q.pop();
+		for (auto k : { cur+ d, cur - e }) {
+			if (k > a || k <= 0 || val[k] == 1) continue;
+			tower[k] = tower[cur] + 1;
+			val[k] = true;
+			q.push(k);
+		}
+	}
+	if (val[c] != 0) cout << tower[c];
+	else cout << "use the stairs";
+}
