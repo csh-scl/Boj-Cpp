fe79b4f07588db7706268e9f7912830e8fe6b00a
csh-scl <140063021+csh-scl@users.noreply.github.com>
2023-08-17 11:06:26 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+ // 값을 저장해 출력하는 배열 index 0에서 부터
+ // 각각의 원소를 사용했는 지 안했는지를 나타내는 배열
+int arr[10];
+bool state[10];
+
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (!state[i]) {
+			if (k >= 1) {
+				if (arr[k - 1] > i) continue;
+			}
+			arr[k] = i;
+			state[i] = 1;
+			func(k + 1);
+			state[i] = 0;
+		}
+	}
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+	func(0);	
+}
