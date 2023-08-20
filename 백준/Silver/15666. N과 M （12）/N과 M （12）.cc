bdb1016c9ec0477f45d191946cb19d18c35591da
csh-scl <140063021+csh-scl@users.noreply.github.com>
2023-08-20 12:46:09 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+bool state[10];
+int num[10];
+
+// 모든 인덱스를 출력하는 코드
+void pr(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << num[arr[i]] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	int st = 0;
+	int tmp = 0;
+	if (k != 0) st = arr[k - 1];
+
+	for (int i = st; i < n; i++) {
+		if (tmp != num[i]) {
+			arr[k] = i;
+			tmp = num[arr[k]];
+			state[i] = 1;
+			pr(k + 1);
+			state[i] = 0;
+		}
+	}
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> num[i];
+	}
+	sort(num, num + n);
+	pr(0);
+}
