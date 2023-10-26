e035bf4aecafde666c3c28536205d0808b5029c6
csh-scl <140063021+csh-scl@users.noreply.github.com>
2023-10-26 10:17:15 +0900

    [Gold V] Title: 퇴사 2, Time: 792 ms, Memory: 72332 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+// arr 배열은 해당 당일의 할 수 있는 일 1. 당일일은 하거나 2. 하루 미루기
+int arr[15000005];
+int work[1500005];
+int value[1500005];
+int main(void){
+	int num;
+	cin >> num;
+	
+	for (int i = 1; i <= num; i++) {
+		cin >> work[i] >> value[i];
+	}
+	
+	for (int i = num; i >= 1; i--) {
+		if (i + work[i] <= num+1) {
+			arr[i] = max(arr[i + 1], arr[i + work[i]] + value[i]);
+		}
+		else
+			arr[i] = arr[i+1];
+	}
+
+	cout << *max_element(arr, arr+num+1);
+} 
