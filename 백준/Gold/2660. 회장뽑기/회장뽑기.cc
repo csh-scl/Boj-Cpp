842d6790da41361efc00ab69917b31f995740afd
csh-scl <140063021+csh-scl@users.noreply.github.com>
2023-11-14 23:11:45 +0900

    [Gold V] Title: 회장뽑기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2660. 회장뽑기/회장뽑기.cc
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.cc
@@ -24,7 +24,7 @@ int bfs(int st) { // st의 점수 계산
             q.push(fr);
         }
     }
-    int val = *max_element(dist + 1, dist + n + 1); // 가장 거리가 먼 사람
+    int val = *max_element(dist, dist + n + 1); // 가장 거리가 먼 사람
     if (val == 0) return 0; // 친구가 아예 없으면 val은 0이 됨
     return val;
 }
