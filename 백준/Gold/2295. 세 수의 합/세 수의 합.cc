b4d10487fd0db67474938a8d75f516d089567e29
Hyeon <140063021+csh-scl@users.noreply.github.com>
2024-01-06 22:28:28 +0900

    [Gold IV] Title: 세 수의 합, Time: 28 ms, Memory: 5228 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.cc
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -1,36 +1,30 @@
+// Authored by : BaaaaaaaaaaarkingDog
+// Co-authored by : -
+// http://boj.kr/f19cd7f505434920a0a4a9aa86454364
 #include <bits/stdc++.h>
 using namespace std;
 
+int n;
+int a[1005];
+vector<int> two;
+
 int main(void) {
-
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int n;
-	cin >> n;
-	vector<int> v;
-	for (int i = 0; i < n; i++) {
-		int x;
-		cin >> x;
-		v.push_back(x);
-	}
-
-	sort(v.begin(), v.end());
-
-	vector<int> two;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			two.push_back(v[i] + v[j]);
-		}
-	}
-
-	sort(two.begin(), two.end());
-
-	for (int i = n - 1; i >= 0; i--) {
-		for (int j = 0; j < n; j++) {
-			if (binary_search(two.begin(), two.end(), v[i] - v[j])) {
-				cout << v[i];
-				return 0;
-			}
-		}
-	}
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  for(int i = 0; i < n; i++) cin >> a[i];
+  sort(a, a+n);
+  for(int i = 0; i < n; i++){
+    for(int j = i; j < n; j++)
+      two.push_back(a[i]+a[j]);
+  }
+  sort(two.begin(), two.end());
+  for(int i = n-1; i > 0; i--){
+    for(int j = 0; j < i; j++){
+      if(binary_search(two.begin(), two.end(), a[i]-a[j])){
+        cout << a[i];
+        return 0;
+      }
+    }
+  }
 }
