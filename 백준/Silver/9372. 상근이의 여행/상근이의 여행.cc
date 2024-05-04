d35bc7b3ae9a14f4916e38dccc033d3ff4bf76ce
Hyeon <140063021+csh-scl@users.noreply.github.com>
2024-05-05 01:33:52 +0900

    [Silver IV] Title: 상근이의 여행, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+priority_queue<int, vector<int>> pq;
+
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int x, y;
+		cin >> x >> y;
+		for (int i = 1; i <= y; i++) {
+			int a, b;
+			cin >> a >> b;
+		}
+
+		cout << x - 1 << '\n';
+	}
+}
