41596d03b6e9c5ef795f1e3488c4b80affba7463
Hyeon <140063021+csh-scl@users.noreply.github.com>
2024-05-04 16:04:04 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 64 ms, Memory: 9736 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,43 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
-// <비용, 정점>
-vector<pair<int, int>> adj[10005];
+
+// 비용, 출발, 도착지
+priority_queue < tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
+
+int n, m;
+vector<pair<int, int>> v[10005];
 bool vis[10005];
 
-int v, e; // 정점 , 간선의 개수
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int x, y, z;
+		cin >> x >> y >> z;
+		// 도착지, 비용 정보 추가
+		v[x].push_back({ y,z });
+		v[y].push_back({ x,z });
+	}
 
-int main()
-{
-    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>> > pq;
-    cin >> v >> e;
-    for (int i = 0; i < e; i++) {
-        int x, y, w;
-        cin >> x >> y >> w;
-        adj[x].push_back(make_pair(w, y));
-        adj[y].push_back(make_pair(w, x));
-    }
+	int cnt = 0;
+	vis[1] = 1;
 
-    int n = 0; // 간선의 갯수
-    int ans = 0;
+	for (auto k : v[1]) {
+		int des = k.first;
+		int pri = k.second;
+		pq.push({ pri, 1, des});
+	}
 
-    vis[1] = 1;
-    for (auto k : adj[1]) {
-        pq.push({ k.first, 1 , k.second });
-    }
+	int ans = 0;
 
-    while (!pq.empty()) {
-        int cost,a,b;
-        tie(cost, a ,b) = pq.top(); pq.pop();
-        if (vis[b]) continue;
-        vis[b] = 1;
-        ans += cost;
-        n++;
-        for (auto k : adj[b]) {
-            if (!vis[k.second])
-                pq.push({k.first, b, k.second});
-        }
+	// 간선이 n-1개가 될 때 까지 반복한다.
+	while (cnt < n - 1) {
+		int cost, st, en;
+		tie(cost, st, en) = pq.top(); pq.pop();
+		if (vis[en]) continue;
+		ans += cost;
+		vis[en] = 1;
+		cnt++;
+		for (auto nxt : v[en]) {
+			pq.push({ nxt.second, en, nxt.first});
+		}
+	}
 
-    }
-    cout << ans;
+	cout << ans;
 }
-
