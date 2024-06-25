02acbf408900a547062ea2988ecc690f82475c3b
Cho Seonghyeon <140063021+csh-scl@users.noreply.github.com>
2024-06-25 11:26:32 +0900

    [Silver I] Title: 그림, Time: 20 ms, Memory: 3252 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,50 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define X first
-#define Y second
 
 int board[502][502];
 bool vis[502][502];
+
 int dx[4] = { 1,0,-1,0 };
 int dy[4] = { 0,1,0,-1 };
+
 int n, m;
 
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> board[i][j];
-		}
-	}
-	int count = 0;
-	int s = 0;
-	
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (board[i][j] == 0 || vis[i][j]) continue;
-			
-			count++;
-			queue<pair<int, int> > Q;
-			Q.push({ i, j });
-			vis[i][j] = 1;
-			int area = 0;
-			while (!Q.empty()) {
-				area++;
-				pair<int, int> cur = Q.front(); Q.pop();
-				for (int dir = 0; dir < 4; dir++) {
-					int nx = cur.X + dx[dir];
-					int ny = cur.Y + dy[dir];
-					if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-					if (vis[nx][ny] || board[nx][ny] != 1) continue;
-					Q.push({ nx,ny });
-					vis[nx][ny] = 1;
-				}
-			}
-			s = max(s, area);
-		}
-	}
-	cout << count << '\n' << s;
+queue<pair<int, int>> q;
+
+int bfs(int x, int y) {
+    int count = 1;
+    q.push({ x,y });
+    vis[x][y] = 1;
+    while (!q.empty()) {
+        auto cur = q.front();
+        q.pop();
+        for (int i = 0; i < 4; i++) {
+            int nx = cur.first + dx[i];
+            int ny = cur.second + dy[i];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if (vis[nx][ny] == 1 || board[nx][ny] != 1) continue;
+            q.push({ nx,ny });
+            count++;
+            vis[nx][ny] = 1;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int max_cnt = 0;
+    int value = 0;
+    cin.sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (vis[i][j] != 1 && board[i][j] == 1) {
+                value++;
+                max_cnt = max(max_cnt,bfs(i, j));
+            }
+        }
+    }
+
+    cout << value << '\n' << max_cnt;
+    
+    
+
 }
