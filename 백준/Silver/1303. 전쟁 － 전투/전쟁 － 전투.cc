217e1ebe391f3a73545ba6c0d3d1858aeaa83ff3
Cho Seonghyeon <140063021+csh-scl@users.noreply.github.com>
2025-01-14 18:08:22 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.cc
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.cc
@@ -1,69 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
 string s[105];
 bool m[105][105];
 
 int wcnt;
 int bcnt;
+int x, y;
+
+// BFS 함수: 시작 좌표 (sx, sy)와 목표 문자 target을 인자로 받음
+int bfs(int sx, int sy, char target) {
+    queue<pair<int, int>> q;
+    q.push({ sx, sy });
+    m[sx][sy] = true;
+    int cnt = 1;
+
+    while (!q.empty()) {
+        auto cur = q.front(); q.pop();
+        for (int d = 0; d < 4; d++) {
+            int nx = cur.first + dx[d];
+            int ny = cur.second + dy[d];
+            if (nx < 0 || nx >= x || ny < 0 || ny >= y) continue;
+            if (!m[nx][ny] && s[nx][ny] == target) {
+                q.push({ nx, ny });
+                m[nx][ny] = true;
+                cnt++;
+            }
+        }
+    }
+    return cnt * cnt; // 크기의 제곱 반환
+}
 
 int main() {
-	int x, y;
-	cin >> y >> x;
-	// y 가로,  x 세로
-	for (int i = 0; i < x; i++) {
-		cin >> s[i];
-	}
-	// 입력 처리
-	queue<pair<int,int>> b;
-	queue<pair<int, int>> w;
-	// x 세로, y 가로
-	for (int i = 0; i < x; i++) {
-		for (int j = 0; j < y; j++) {
-			if (s[i][j] == 'W' && m[i][j] == 0) {
-				int cnt = 1;
-				w.push({ i,j });
-				m[i][j] = true;
-				while (!w.empty()) {
-					auto cur = w.front(); w.pop();
-					for (int i = 0; i < 4; i++) {
-						int nx = cur.first + dx[i];
-						int ny = cur.second + dy[i];
-						if (nx < 0 || nx >= x || ny < 0 || ny >= y) continue;
-						if (m[nx][ny] == 0 && s[nx][ny] == 'W') {
-							w.push({ nx,ny });
-							m[nx][ny] = true;
-							cnt++;
-						}
-					}
-				}
-				wcnt += cnt * cnt;
-			}
+    
+    cin >> y >> x; // y: 가로, x: 세로
+    for (int i = 0; i < x; i++) {
+        cin >> s[i];
+    }
 
-			if (s[i][j] == 'B' && m[i][j] == 0) {
-				int cnt = 1;
-				b.push({ i,j });
-				m[i][j] = true;
-				while (!b.empty()) {
-					auto cur = b.front(); b.pop();
-					for (int i = 0; i < 4; i++) {
-						int nx = cur.first + dx[i];
-						int ny = cur.second + dy[i];
-						if (nx < 0 || nx >= x || ny < 0 || ny >= y) continue;
-						if (m[nx][ny] == 0 && s[nx][ny] == 'B') {
-							b.push({ nx,ny });
-							m[nx][ny] = true;
-							cnt++;
-						}
-					}
-				}
-				bcnt += cnt * cnt;
-			}
-		}
-	}
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+            if (s[i][j] == 'W' && !m[i][j]) {
+                wcnt += bfs(i, j, 'W');
+            }
+            else if (s[i][j] == 'B' && !m[i][j]) {
+                bcnt += bfs(i, j, 'B');
+            }
+        }
+    }
 
-	cout << wcnt << ' ' << bcnt;
-
+    cout << wcnt << ' ' << bcnt;
+    return 0;
 }
