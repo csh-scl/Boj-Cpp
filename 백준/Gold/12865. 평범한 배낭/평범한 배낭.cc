7c99bf2ccf69de800d19d9d8f333779fa35acc2f
Cho Seonghyeon <140063021+csh-scl@users.noreply.github.com>
2025-03-24 09:43:46 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,36 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[16];
-
-int weight[102];
-int value[102];
-
-
-
-int res[102][100002];
-
-// dp[i][j] i번째 물건까지 고려해 무게가 j일때 만들 수 있는 최대 가치 
+int weight[102], value[102];
+int res[100001]; // 1차원 DP 배열 (무게 j까지 담을 때 최대 가치)
 
 int main() {
-    
-    // 개수, 무게
     int n, m;
     cin >> n >> m;
+
     for (int i = 1; i <= n; i++) {
         cin >> weight[i] >> value[i];
     }
 
+    // 1차원 DP 최적화 (거꾸로 순회)
     for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            if (weight[i] <= j) {
-                res[i][j] = max(res[i - 1][j], res[i - 1][j - weight[i]] + value[i]);
-            }
-            else {
-                res[i][j] = res[i - 1][j];
-            }
+        for (int j = m; j >= weight[i]; j--) { 
+            res[j] = max(res[j], res[j - weight[i]] + value[i]);
         }
-   }
+    }
 
-    cout << res[n][m];
+    cout << res[m]; // 최대 가치 출력
 }
