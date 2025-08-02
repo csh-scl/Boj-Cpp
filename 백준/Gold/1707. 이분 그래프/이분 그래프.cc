efdfcf173b3973fffae3d8285391552cc4c81dfb
Cho Seonghyeon <140063021+csh-scl@users.noreply.github.com>
2025-08-02 21:29:02 +0900

    [Gold IV] Title: 이분 그래프, Time: 196 ms, Memory: 11368 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.cc
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.cc
@@ -3,66 +3,74 @@ using namespace std;
 
 
 
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
-	int tt;
-	cin >> tt;
 
-	for (int i = 0; i < tt; i++) {
-		int no, ed;
-		cin >> no >> ed;
-		vector<int> grp[20002];
-		int vis[20002];
-		queue<int> q;
-		fill(vis, vis + 20002, -1);
-		
-		bool isTrue = true;
+int vis[20002];
+vector<int> adj[20002];
+int no, eg;
 
-		for (int i = 0; i < ed; i++) {
-			int x, y;
-			cin >> x >> y;
-			grp[x].push_back(y);
-			grp[y].push_back(x);
-		}
+bool bfs() {
+    fill(vis, vis + no+1 , -1);
+    queue<int> q;
+    for (int i = 1; i <= no; i++) {
+        if (vis[i] != -1) {
+            continue;
+        }
+        else {
+            q.push(i);
+            vis[i] = 0;
 
-		for (int i = 1; i < no; i++) {
-			if (vis[i] == -1) {
-				q.push(i);
-				vis[i] = 1;
-				while (!q.empty()) {
-					int cur = q.front();
-					q.pop();
-					for (auto k : grp[cur]) {
-						if (vis[k] == -1) {
-							q.push(k);
-							if (vis[cur] == 1) {
-								vis[k] = 0;
-							}
-							else {
-								vis[k] = 1;
-							}
-						}
-						else {
-							if (vis[k] == vis[cur]) {
-								cout << "NO" << '\n';
-								isTrue = false;
-								break;
-							}
-						}
-					}
-					if (!isTrue) {
-						break;
-					}
-				}
-				if (!isTrue) {
-					break;
-				}
-			}
-		}
-		if (isTrue) {
-			cout << "YES" << '\n';
-		}
-	}
+            while (!q.empty()) {
+                int cur = q.front(); // 3번
+                q.pop();
+                for (auto k : adj[cur]) {
+                    if (vis[k] != -1) {
+                        if (vis[k] == vis[cur]) {
+                            return false;
+                        }
+                        else {
+                            continue;
+                        }
+                    }
+                    else {
+                        vis[k] = (vis[cur] + 1) % 2;
+                        q.push(k);
+                    }
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int k;
+    cin >> k;
+    while (k--) {
+
+        cin >> no >> eg;
+
+        for (int i = 1; i <= no; i++)
+            adj[i].clear();
+
+        for (int i = 0; i < eg; i++) {
+            int x, y;
+            cin >> x >> y;
+            adj[x].push_back(y);
+            adj[y].push_back(x);
+        }
+
+        if (bfs()) {
+            cout << "YES" << '\n';
+        }
+        else {
+            cout << "NO" << '\n';
+        }
+    }
+
+
 }
